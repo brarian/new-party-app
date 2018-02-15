79b743c634cb04ae1353101653a54e682a115e8b
brarian <b.namonywa@gmail.com>
2018-02-14 22:47:49 -0600

    landing page changes/ border radius

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -5,7 +5,7 @@ import cheers from '../images/cheers.jpg';
 import balloon from '../images/balloon.png';
 import pencil from '../images/pencil.png';
 import execute from '../images/execute.png';
-
+import brunch from '../images/brunch.jpg';
 
 class LandingPage extends React.Component {
 
@@ -15,19 +15,20 @@ class LandingPage extends React.Component {
 			<div >
 <div className="small-12 medium-6 columns landing-bg"></div>
 <div>
-<img src={cheers} id="bg" alt="people cheer'sing with drinks"/>
+<img src={brunch} alt="people cheer'sing with drinks"/>
 </div>
-	<h1>
+	<div className="header">
 		party planning made easy
-	</h1>
+	</div>
 	<div >
-		<button className="small-4 small-offset-4 medium-2 medium-offset-5 columns signUpButton button large"> <Link to="/newUser">Join Us!</Link></button>
+		<button className="small-4 small-offset-4 medium-2 medium-offset-3 columns signUpButton button large"> <Link to="/newUser">Join Us!</Link></button>
 	</div>
 	<div>
-	<div className="grid-x small-up-1 small-offset-3 medium-up-3 medium-offset-1 symbol">
-		<div className="column"><img src={pencil} alt="icon of a pencil" className="sym"/><p>1. Plan  </p></div>
-		<div className="column "><img src={execute} alt="icon of a checklist" className="sym"/><p>2. Execute</p></div>
-		<div className="column "><img src={balloon} alt="icon of a balloon" className="sym"/><p>3. Enjoy </p></div>
+	<div  className="heading">How Little Mix works </div>
+	<div className="grid-x small-up-1 small-offset-3 medium-up-3 medium-offset-0 symbol">
+		<div className="landingBox"><img src={pencil} alt="icon of a pencil" className="sym"/><p><span className="head">Plan</span> </p> Choose what type of party you’d like to have.</div>
+		<div className="landingBox "><img src={execute} alt="icon of a checklist" className="sym"/><p><span className="head">Execute</span></p> Keep track of what needs to be done and when it needs to be done.</div>
+		<div className="landingBox "><img src={balloon} alt="icon of a balloon" className="sym"/><p><span className="head">Entertain</span> </p>Join your guests in enjoying the wonderful party you’ve planned! </div>
 	</div>
 	</div>
 			</div>
